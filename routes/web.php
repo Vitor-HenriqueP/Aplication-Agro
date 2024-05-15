5f0459d55d593bc58181d96cc97d65407700d5c0
Vitor <engs-vitorpachinski@camporeal.edu.br>
2024-05-15 18:45:36 -0300

    atualizado

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('products', ProductController::class)
